0e01b2ac22f5571d2a1bd8c0ac12ae53c17c736d
marioverdu <marioverdugambin@gmail.com>
2022-12-22 20:13:35 +0100

    Delete framerusercontent.com directory

--- framerusercontent.com/sites/25nikOx1wOLfM8E2WIMZhL/chunk-BRO6AIRF.mjs
+++
@@ -1,2 +0,0 @@
-function e(t){return typeof t=="string"?t:String(t)}var a=t=>({breakpoints:[{hash:"tzgt0p"}],elements:{},title:`${t?.CJxyD1BEF!==void 0?e(t.CJxyD1BEF):"{{CJxyD1BEF}}"} - Mario Verd\xFA`,viewport:"width=800"}),i=a,d=1,s={exports:{default:{type:"variable",annotations:{framerContractVersion:"1"}},metadataVersion:{type:"variable",annotations:{framerContractVersion:"1"}},__FramerMetadata__:{type:"variable"}}};export{i as a,d as b,s as c};
-//# sourceMappingURL=chunk-BRO6AIRF.mjs.map
