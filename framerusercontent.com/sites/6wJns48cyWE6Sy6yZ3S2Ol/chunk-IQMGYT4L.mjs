89de8924ada7763c4507bf3b6d82d75b98676a29
marioverdu <mr.mariets@gmail.com>
2023-01-16 22:32:59 +0100

    come back

---
+++ framerusercontent.com/sites/6wJns48cyWE6Sy6yZ3S2Ol/chunk-IQMGYT4L.mjs
@@ -0,0 +1,2 @@
+var a=e=>({description:"Digital Product Designer",favicon:new URL("https://framerusercontent.com/images/Ar0akU5zunFv2ReBnq8LxBNWYh0.png").href,socialImage:new URL("https://framerusercontent.com/images/Ar0akU5zunFv2ReBnq8LxBNWYh0.png").href,title:"Mario Verd\xFA"}),o=a;export{o as a};
+//# sourceMappingURL=chunk-IQMGYT4L.mjs.map
