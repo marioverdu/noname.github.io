5a0ad84708398b47909ee14b0fbb7f9c374b77f4
marioverdu <mr.mariets@gmail.com>
2023-05-09 08:35:26 +0200

    v15.3.1
    idk

--- framerusercontent.com/sites/6wJns48cyWE6Sy6yZ3S2Ol/chunk-3YMVWWLW.mjs
+++
@@ -1,2 +0,0 @@
-import{a as t}from"./chunk-IQMGYT4L.mjs";var e=a=>({breakpoints:[{hash:"72rtr7",mediaQuery:"(min-width: 800px)"},{hash:"qalnr8",mediaQuery:"(min-width: 600px) and (max-width: 799px)"},{hash:"21mkqg",mediaQuery:"(max-width: 599px)"}],elements:{},title:t(a).title||"Home",viewport:"width=device-width"}),d=e,m=1,s={exports:{default:{type:"variable",annotations:{framerContractVersion:"1"}},metadataVersion:{type:"variable",annotations:{framerContractVersion:"1"}},__FramerMetadata__:{type:"variable"}}};export{d as a,m as b,s as c};
-//# sourceMappingURL=chunk-3YMVWWLW.mjs.map
